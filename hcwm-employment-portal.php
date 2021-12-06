0682b05efd3b63f9ccb0030607f98375d64e4421
JGebhart <33806238+JGebhart@users.noreply.github.com>
2021-12-06 17:18:43 -0500

    0.0.0

---
+++ hcwm-employment-portal.php
@@ -0,0 +1,12 @@
+<?php
+/*
+	Plugin Name: HCWM Employment Portal
+	Plugin URI: NA
+	Description: Plugin to add the employment portal / management to the HCWM web site
+	Author: JCG Solutions, LLC.
+	Version: 0.0.0
+	Author URI: https://jcgsolutions.com
+	License: GPL2
+	GitHub Plugin URI: JCGSolutions/HCWM-Employment-Portal
+*/
+?>
